21688a0b3f6d2e59990c73e89f54e82cea21f4d3
NicoChlebo <nico.chlebo@gmail.com>
2025-02-26 11:35:59 +0100

    remove whitespace

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def divide(x, y):
     """Divise x par y en vérifiant que y n'est pas nul."""
     if y != 0:
         return x / y
-    return None 
+    return None
 
 
 def greet(name):
     """Génère un message de salutation."""
     if name == "":
         return "Hello, World!"
-    return f"Hello, {name}" 
+    return f"Hello, {name}"
