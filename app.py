f207ea8430f3e370462987523fbc995750775346
NicoChlebo <nico.chlebo@gmail.com>
2025-02-26 11:33:35 +0100

    wip for linter

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ def add(a, b):
     """Ajoute deux nombres."""
     return a + b
 
-def multiply( x,y ):
-    return x*y
 
-def divide(x , y ):
-    beta = 0
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    beta = 0  # Variable inutilisée, tu peux la supprimer si elle n'est pas nécessaire
     if y != 0:
-        return x/y
+        return x / y
+
 
 def greet(name):
-    # GREET FONCTION
-    if name =="":
+    """Génère un message de salutation."""
+    if name == "":
         return "Hello, World!"
     else:
-       return "Hello,"+name
+        return "Hello, " + name
